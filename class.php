1c2e550da1088f59dedfe378388837be4803e965
unknown <niputuayunoviwidiarti@gmail.com>
2023-03-02 15:02:52 +0800

    firt commit

---
+++ class.php
@@ -0,0 +1,15 @@
+<?php
+class luas{
+
+    public function segitiga($alas,$tinggi){
+       return $alas * $tinggi /2;
+     }
+   
+}
+   
+   $luas = new Luas();
+   
+   $alas = 6;
+        $tinggi = 15;
+        echo $luas->segitiga($alas,$tinggi);
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+  echo "Menghitung Luas Segitiga";
+  echo "<hr>";
+ 
+  $alas = 6;
+  $tinggi = 15;
+ 
+  $luas_segitiga = 1/2 * $alas * $tinggi;
+ 
+  echo "alas = $alas <br>";
+  echo "tinggi = $tinggi <br>";
+  echo "luas = ". round($luas_segitiga) ."<br>";
+?>
